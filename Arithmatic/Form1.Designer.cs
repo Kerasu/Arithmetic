afc303e4dc2d52ec1ddfef0be6271226839d740e
Kyou <RazeGT@hotmail.com>
2019-04-13 22:51:39 -0400

    Add project files.

---
+++ Arithmatic/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Arithmatic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnAddFloats = new System.Windows.Forms.Button();
+            this.btnSubtraction = new System.Windows.Forms.Button();
+            this.btnMixed = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(83, 32);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(110, 30);
+            this.btnadd.TabIndex = 0;
+            this.btnadd.Text = "Integer - Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.Btnadd_Click);
+            // 
+            // btnAddFloats
+            // 
+            this.btnAddFloats.Location = new System.Drawing.Point(83, 91);
+            this.btnAddFloats.Name = "btnAddFloats";
+            this.btnAddFloats.Size = new System.Drawing.Size(110, 30);
+            this.btnAddFloats.TabIndex = 1;
+            this.btnAddFloats.Text = "Add - Floats";
+            this.btnAddFloats.UseVisualStyleBackColor = true;
+            this.btnAddFloats.Click += new System.EventHandler(this.BtnAddFloats_Click);
+            // 
+            // btnSubtraction
+            // 
+            this.btnSubtraction.Location = new System.Drawing.Point(83, 149);
+            this.btnSubtraction.Name = "btnSubtraction";
+            this.btnSubtraction.Size = new System.Drawing.Size(110, 32);
+            this.btnSubtraction.TabIndex = 2;
+            this.btnSubtraction.Text = "Subtract";
+            this.btnSubtraction.UseVisualStyleBackColor = true;
+            this.btnSubtraction.Click += new System.EventHandler(this.BtnSubtraction_Click);
+            // 
+            // btnMixed
+            // 
+            this.btnMixed.Location = new System.Drawing.Point(83, 210);
+            this.btnMixed.Name = "btnMixed";
+            this.btnMixed.Size = new System.Drawing.Size(110, 35);
+            this.btnMixed.TabIndex = 3;
+            this.btnMixed.Text = "Add and Subtract";
+            this.btnMixed.UseVisualStyleBackColor = true;
+            this.btnMixed.Click += new System.EventHandler(this.BtnMixed_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 394);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMixed);
+            this.Controls.Add(this.btnSubtraction);
+            this.Controls.Add(this.btnAddFloats);
+            this.Controls.Add(this.btnadd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnAddFloats;
+        private System.Windows.Forms.Button btnSubtraction;
+        private System.Windows.Forms.Button btnMixed;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Arithmatic/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arithmatic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnadd_Click(object sender, EventArgs e)
+        {
+            int firstNumber;
+            int secondNumber;
+            int integerAnswer;
+
+            firstNumber = 10;
+            secondNumber = 32;
+
+            integerAnswer = 100 + firstNumber + secondNumber + 327489847;
+            MessageBox.Show(integerAnswer.ToString(),"Addition Box");
+        }
+
+        private void BtnAddFloats_Click(object sender, EventArgs e)
+        {
+            float firstNumber;
+            float secondNumber;
+            float floatAnswer;
+            int integerAnswer = 20;
+
+            firstNumber = 10.5F;
+            secondNumber = 32.5F;
+
+            floatAnswer = firstNumber + secondNumber + integerAnswer;
+
+            MessageBox.Show(floatAnswer.ToString(), "Float Box");
+        }
+
+        private void BtnSubtraction_Click(object sender, EventArgs e)
+        {
+            int answerSubtract;
+            int numberOne = 12;
+            int numberTwo = -55;
+
+            answerSubtract = 50 - 25 - 10 - 2 - numberOne - numberTwo;
+
+            MessageBox.Show(answerSubtract.ToString(), "Subtraction Box");
+        }
+
+        private void BtnMixed_Click(object sender, EventArgs e)
+        {
+            int firstNumber;
+            int secondNumber;
+            int thirdNumber;
+            int fourthNumber;
+            int fifthNumber;
+            int answer;
+
+            firstNumber = 100;
+            secondNumber = 50;
+            thirdNumber = -50;
+            fourthNumber = 10 / 5;
+            fifthNumber = 2 * 2;
+
+            answer = ((firstNumber + secondNumber - thirdNumber) / fourthNumber) * fifthNumber;
+
+            MessageBox.Show(answer.ToString());
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
